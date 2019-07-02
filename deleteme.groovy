3f95ff021f2822e4e94ccba2a6a9f27fef6a810b
Natan7878 <seg.natan@gmail.com>
2019-07-02 15:42:41 -0400

    Update deleteme.groovy

--- deleteme.groovy
+++ deleteme.groovy
@@ -1,20 +1,24 @@
 pipeline{
     agent any
     stages {
-        stage ('Build'){
-            tools {
-                maven 'localMaven'
-            }
+        stage ('Build') {
             steps {
                 sh 'mvn clean package'
             }
             post {
                 success {
                     echo 'Now Archiving...'
-                    archiveArtifacts artifacts: '**/target/*.war'
+                    archiveArctifacts artifacts: '**/target/*.war'
                 }
             }
 
         }
+        stage ('Deply to staging ....'){
+            steps{
+                build job: 'deploy-to-staging'
+            }
+        }
+
+
     }
 }
