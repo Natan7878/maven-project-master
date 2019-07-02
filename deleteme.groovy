4bcda9450292c275b4db208170a534fc369df713
Natan7878 <seg.natan@gmail.com>
2019-07-02 15:46:14 -0400

    Update deleteme.groovy

--- deleteme.groovy
+++ deleteme.groovy
@@ -2,6 +2,9 @@ pipeline{
     agent any
     stages {
         stage ('Build') {
+            tools{
+                maven 'localMaven'
+            }
             steps {
                 sh 'mvn clean package'
             }
