fad99ff09e29b13d2ea78a99ecbe10934d29bbae
Natan7878 <seg.natan@gmail.com>
2019-07-03 14:28:59 -0400

    Update dockerfile.groovy

--- dockerfile.groovy
+++ dockerfile.groovy
@@ -7,7 +7,7 @@ pipeline{
             }
             steps{
                 sh 'mvn clean package'
-                sh 'docker build . -t tomcatwebapp:${env.BUILD_ID}'
+                sh "docker build . -t tomcatwebapp:${env.BUILD_ID}"
             }
         }
     }
