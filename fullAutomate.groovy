c5c079b9c75c143c1598f7c2e9ce3637c745dc76
Natan7878 <seg.natan@gmail.com>
2019-07-03 11:33:18 -0400

    add Jenkins file for full automation with AWS

---
+++ fullAutomate.groovy
@@ -0,0 +1,50 @@
+pipeline {
+    agent any
+
+    parameters{
+        string(name: 'tomcate_todev', defaultValue: '35.166.210.154', description: 'Staging Server')
+        string(name: 'tomcat_prod', defaultValue: '34.209.233.6', description: 'Production Server')
+    }
+
+    triggers {
+        pollSCM(' * * * * *')
+    }
+
+    stages {
+        stage('Buld'){
+            tools {
+                maven 'localMaven'
+            }
+            steps{
+                sh 'mvn clean package'
+            }
+
+            post {
+                success {
+                    echo 'Now Archiving....'
+                    archiveArtifacts artifacts: '**/target/*.war'
+                }
+            }
+        }
+
+        stage ('Deployments'){
+            parallel {
+                stage ('Deploy to staging '){
+                    steps {
+                        sh "scp -i /home/jenkins/tomcat-demo.pem **/target/*.war ec2-user@${params.tomcat_dev}:/var/lib/tomcat7/webapps"
+
+                    }
+                }
+
+                stage ("Deploy to Production") {
+                    steps {
+                        sh "scp -i /home/jenkins/tomcat-demo.pem **/target/*.war ec2-user@${params.tomcat_prod}:/var/lib/tomcat7/webapps"
+
+                    }
+                }
+            }
+
+        }
+    }
+
+}
